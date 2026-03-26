48413f9f7243be1c03497bee47ceeaaa5e173d35
Joao-Souza-493 <joaop.souza.493@gmail.com>
2026-03-26 17:10:07 -0300

    remove.py

---
+++ 01_pre_processamento/remove.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import csv
+from obspy import read
+
+os.makedirs('/home/joao/Documentos/IC/IC/INV/DATA/DATA/OUTSIDE', exist_ok=True)
+pasta = '/home/joao/Documentos/IC/IC/INV/DATA/DATA/'
+
+arquivos = os.listdir(pasta)
+
+with open('coordenads.csv', 'w', newline='') as arquivo_csv:
+	escritor_csv = csv.writer(arquivo_csv)
+	escritor_csv.writerow(['nome', 'Latitude', 'Longitude'])
+
+	for arquivo in arquivos:
+		if arquivo.endswith(('.z','.t','.r')):
+			st = read(os.path.join(pasta, arquivo), debug_headers=True)
+			distancia = st[0].stats.sac.dist
+
+			if distancia > 1110:
+                
+				if arquivo in ('br_guaj_dsp.r', 'br_guaj_dsp.t', 'br_guaj_dsp.z'):
+					dados = st[0].stats.sac.knetwk, st[0].stats.sac.stla, st[0].stats.sac.stlo
+					escritor_csv.writerow(dados)
+				else:
+					shutil.move(os.path.join(pasta, arquivo), '/home/joao/Documentos/IC/IC/INV/DATA/DATA/OUTSIDE')
+	    
+			else: 
+				dados = st[0].stats.sac.knetwk, st[0].stats.sac.stla, st[0].stats.sac.stlo
+				escritor_csv.writerow(dados)
